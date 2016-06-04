5cc83448a15e7e9befd8e66c361a3106fe2c3a90
saptarshi <saptarshidutta31@yahoo.co.in>
2016-06-04 23:47:47 +0530

    adding visual widget carousel to the home screen

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var app = express();
 /**
  * Express configuration.
  */
-app.set('port', process.env.PORT || 3030);
+app.set('port', process.env.PORT || 8888);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', mustacheExpress());          // register file extension mustache
 app.set('view engine', 'html');                 // register file extension for partials
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'my-list-frontEnd'), { maxAge: 31557
  */
 app.get('/', homeController);
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'),'192.168.0.5', function() {
 	  console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
 	});
 module.exports = app;
